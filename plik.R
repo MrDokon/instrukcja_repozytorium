d7f669f4c061c25fcbfc5746bbc24711dfa9b5c2
MrDokon <mrdokon@gmail.com>
2022-05-10 20:50:53 +0200

    drugi commit

--- plik.R
+++ plik.R
@@ -1,2 +1,4 @@
 #wszystkie wprowadzone zmiany commitujemy - zapisujemy lokalnie na komputerze
 12+1
+
+#zmiany nie są jeszcze widoczne w naszym repozytorium, aby były należy najpierw zapisać lokalnie - commit, a później dać push

41e9bf214712a082b214af09755e01fa5eaff7af
MrDokon <mrdokon@gmail.com>
2022-05-10 20:53:25 +0200

    czwarty commit

--- plik.R
+++ plik.R
@@ -4,3 +4,5 @@
 #zmiany nie są jeszcze widoczne w naszym repozytorium, aby były należy najpierw zapisać lokalnie - commit, a później dać push
 
 # jeśli wprowadzimy jakieś zmiany w zdalnym repozytorium lub repozytorium połączonym z naszym w innym miejscu, zawsze należy dać pull z poziomu rstudio. 
+
+#czasami pojawiają się konflikty - występują, gdy robobimy coś w złej kolejności

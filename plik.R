bd0185b59e6921013aa35827a0cd223f957f96d9
MrDokon <83066259+MrDokon@users.noreply.github.com>
2022-05-10 20:52:20 +0200

    Update plik.R
    trzeci commit - zdalne repozytorium

--- plik.R
+++ plik.R
@@ -2,3 +2,5 @@
 12+1
 
 #zmiany nie są jeszcze widoczne w naszym repozytorium, aby były należy najpierw zapisać lokalnie - commit, a później dać push
+
+# jeśli wprowadzimy jakieś zmiany w zdalnym repozytorium lub repozytorium połączonym z naszym w innym miejscu, zawsze należy dać pull z poziomu rstudio. 
